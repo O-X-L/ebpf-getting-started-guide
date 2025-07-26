596cc082b7c1cc90d8dba9d5ef8ea0bfe6e1ee2a
Rath Pascal <rath@oxl.at>
2025-07-26 14:00:33 +0200

    extend guide, add funding info

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ build_raw:
 	bash scripts/build_raw.sh
 
 build_go:
-	bash scripts/build_go.sh
+	bash scripts/build_go_single.sh
+
+build_go_all:
+	bash scripts/build_go_all.sh
---
+++ scripts/build_go_single.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ -z "$1" ]
+then
+  SRC_DIR='src/'
+else
+  SRC_DIR="$1"
+fi
+
+set -u
+
+cd "$(dirname "$0")/.."
+
+mkdir -p ./build/
+
+echo '### PREPARING ###'
+if [[ "$(docker images -f reference=ebpf-go-builder-dev | wc -l)" == "1" ]]
+then
+  docker build -t ebpf-go-builder-dev -f ./docker/Dockerfile_build .
+  docker run -v "$(pwd)":/repo ebpf-go-builder-dev /bin/bash -c "\
+    bpftool btf dump file /sys/kernel/btf/vmlinux format c > /repo/build/vmlinux.h && \
+    go mod tidy"
+fi
+
+OUT_FILE='build/ebpf-module'
+
+# see also: "//go:generate" in main.go
+docker run --rm -v "$(pwd)":/repo ebpf-go-builder-dev /bin/bash -c "\
+    cd /repo/${SRC_DIR} && \
+    echo '### GENERATING ###' && \
+    go generate && \
+    echo '### BUILDING ###' && \
+    go build -C /repo/${SRC_DIR} -o '/repo/${OUT_FILE}' -buildvcs=false"
+
+echo "DONE: ${OUT_FILE}"
